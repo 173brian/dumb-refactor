68b61f53b5204767e2193ff6b449872e83f87e17
Brian Anderson <173brian@gmail.com>
2024-05-13 17:20:55 -0500

    Reading file content.

--- lua/dumb-refactor/init.lua
+++ lua/dumb-refactor/init.lua
@@ -1,8 +1,14 @@
 print("hello world")
 
+local function buffer_to_string()
+	local content = vim.api.nvim_buf_get_lines(0, 0, vim.api.nvim_buf_line_count(0), false)
+	return table.concat(content, "\n")
+end
+
 local function runner()
 	print("Function running")
 	print(vim.fn.getcwd())
+	print(buffer_to_string())
 end
 
 return {
