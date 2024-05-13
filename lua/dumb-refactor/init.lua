e63bb98dc80d43317ed7988f55ce1360664dd781
Brian Anderson <173brian@gmail.com>
2024-05-13 17:36:40 -0500

    Counting whitespace.

--- lua/dumb-refactor/init.lua
+++ lua/dumb-refactor/init.lua
@@ -1,14 +1,26 @@
-print("hello world")
-
 local function buffer_to_string()
 	local content = vim.api.nvim_buf_get_lines(0, 0, vim.api.nvim_buf_line_count(0), false)
 	return table.concat(content, "\n")
 end
 
+local function countLeadingWhitespacePerLine(table)
+	local result = {}
+	for _, str in ipairs(table) do
+		local pattern = "^%s*"
+		local whitespace = string.match(str, pattern)
+		if whitespace then
+			result[str] = #whitespace
+		else
+			result[str] = 0
+		end
+	end
+	return result
+end
+
 local function runner()
 	print("Function running")
 	print(vim.fn.getcwd())
-	print(buffer_to_string())
+	print(countLeadingWhitespacePerLine(buffer_to_string()))
 end
 
 return {
