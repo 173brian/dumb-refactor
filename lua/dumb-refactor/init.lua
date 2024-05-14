efa8e4e57feffa25ba1b67682c28f76297f5d37c
Brian Anderson <173brian@gmail.com>
2024-05-14 12:32:59 -0500

    Debugging selection.

--- lua/dumb-refactor/init.lua
+++ lua/dumb-refactor/init.lua
@@ -8,6 +8,33 @@ local function getLeadingWhitespacePerLine(content)
 	return counts
 end
 
+local function tprint(tbl, indent)
+	if not indent then
+		indent = 0
+	end
+	local toprint = string.rep(" ", indent) .. "{\r\n"
+	indent = indent + 2
+	for k, v in pairs(tbl) do
+		toprint = toprint .. string.rep(" ", indent)
+		if type(k) == "number" then
+			toprint = toprint .. "[" .. k .. "] = "
+		elseif type(k) == "string" then
+			toprint = toprint .. k .. "= "
+		end
+		if type(v) == "number" then
+			toprint = toprint .. v .. ",\r\n"
+		elseif type(v) == "string" then
+			toprint = toprint .. '"' .. v .. '",\r\n'
+		elseif type(v) == "table" then
+			toprint = toprint .. tprint(v, indent + 2) .. ",\r\n"
+		else
+			toprint = toprint .. '"' .. tostring(v) .. '",\r\n'
+		end
+	end
+	toprint = toprint .. string.rep(" ", indent - 2) .. "}"
+	return toprint
+end
+
 local function getSelection(buf, bufferContent)
 	local start_pos = vim.fn.getpos("'<")
 	local end_pos = vim.fn.getpos("'>")
@@ -16,8 +43,8 @@ local function getSelection(buf, bufferContent)
 		vim.api.nvim_err_writeln("Error: Selection spans multiple lines.")
 		return nil
 	end
-	print(start_pos)
-	print(end_pos)
+	tprint(start_pos, 0)
+	tprint(end_pos, 0)
 end
 
 local function runner(input)
